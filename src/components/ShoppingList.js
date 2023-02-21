b7ba56150fcef5d59fa5ea0795953d321fe1d4f3
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-02-21 09:44:18 -0700

    Update item in cart status

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -21,6 +21,17 @@ function ShoppingList() {
     setItems([...items, newItem])
   }
 
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map(item => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
@@ -37,7 +48,10 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item 
+          key={item.id} 
+          item={item}
+          onUpdateItem={handleUpdateItem} />
         ))}
       </ul>
     </div>
